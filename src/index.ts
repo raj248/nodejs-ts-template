6e7836b35e2983d11970a6e72f5007581cb16a9c
raj248 <shashankraj007281@gmail.com>
2025-09-11 11:54:47 +0530

    Added README.md and dotenvFlow for env variable

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+import dotenvFlow from "dotenv-flow";
 import app from "./server";
+
+dotenvFlow.config({
+  node_env: process.env.NODE_ENV || "development",
+  debug: process.env.NODE_ENV === "development",
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on port ${process.env.PORT || 3000}`);
