720e01eaa5e76430586f2fdaaf3ce17e04907760
raj248 <shashankraj007281@gmail.com>
2025-09-11 12:11:48 +0530

    using express.json() to parse incoming json request

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express, { type Express, type Request, type Response } from "express";
 
 const app: Express = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
