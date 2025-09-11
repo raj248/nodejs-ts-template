1a4c0e82c70f3c24c3d4efc3a9d6a55871fbd366
raj248 <shashankraj007281@gmail.com>
2025-09-11 14:10:58 +0530

    using cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import express, { type Express, type Request, type Response } from "express";
+import cors from "cors";
 
 const app: Express = express();
+
+// app.use(cors({
+// origin: '*', // your frontend origin
+// methods: ['GET','POST','PUT','DELETE'],
+// credentials: true
+// }));
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
