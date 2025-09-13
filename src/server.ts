471305aaedc82e70f2575efebfee6f7624683a91
raj248 <shashankraj007281@gmail.com>
2025-09-13 17:42:43 +0530

    using morgan for logging

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 const app: Express = express();
 
@@ -13,6 +14,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
